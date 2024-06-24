5652cf954406edec17bf09e05c8b68ac124ee2f4
JohnMwatheka <mwathekaj@gmail.com>
2024-06-24 14:25:27 +0300

    update package.json

--- src/app/page.js
+++ src/app/page.js
@@ -108,20 +108,19 @@ export default function Home() {
                 <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-              <button type="button" data-mdb-button-init="" data-mdb-ripple-init="" class="btn btn-outline-secondary btn-rounded  ripple-surface-dark  m-0 border-0 btn-sm" data-mdb-ripple-color="dark" data-mdb-button-initialized="true" aria-pressed="false"><a className="nav-link link" href="/login">
+              <button type="button" data-mdb-button-init="" data-mdb-ripple-init="" class="btn btn-outline-secondary btn-rounded  ripple-surface-dark  m-0 border-0 btn-sm" data-mdb-ripple-color="dark" data-mdb-button-initialized="true" aria-pressed="false">
+              <a className="nav-link link" href="/login">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-              </svg>
-                
-                
+              </svg> Sign in
                 </a></button>
               
               </li>
               <li className="nav-item">
               <button type="button" data-mdb-button-init="" data-mdb-ripple-init="" class="btn btn-outline-secondary btn-rounded  ripple-surface-dark  m-0 border-0 btn-sm" data-mdb-ripple-color="dark" data-mdb-button-initialized="true" aria-pressed="false"><a className="nav-link" href="/register"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
                 <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/></svg></a></button>
+                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/></svg> Sing up</a></button>
               </li>
             </ul>
             
@@ -130,7 +129,7 @@ export default function Home() {
       </nav>
       <main>
        {/* Search Bar */}
-       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '50px', maxWidth: '600px', width: '100%', marginLeft:'30%', marginTop: '30px'}}>
+       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '50px', maxWidth: '600px', width: '100%',  marginTop: '30px'}}>
           <input type="text" placeholder="Search..." style={{ flex: '1', padding: '10px', borderRadius: '4px', marginRight: '0px', border: '1px solid #ccc', maxWidth: '70%' }} />
           <select name="cities" style={{ padding: '10px', borderRadius: '4px', border: '1px solid #ccc', maxWidth: '28%' }}>
             <option value="newyork">New York</option>
