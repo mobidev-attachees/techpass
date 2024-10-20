0a1b9c28ff91dcdc04cc528476d16f47845a78d2
JohnMwatheka <mwathekaj@gmail.com>
2024-10-20 13:50:46 +0300

    made changes to profile page

--- src/app/components/Profille.js
+++ src/app/components/Profille.js
@@ -1,10 +1,13 @@
-//src/app/profile/page.js
+// src/app/profile/page.js
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
 import dynamic from 'next/dynamic';
 import { Calendar } from 'react-bootstrap-icons';
 import toast from "react-hot-toast";
+import 'boxicons/css/boxicons.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Spinner = dynamic(() => import('../components/Spinner'), { ssr: false });
 const Toaster = dynamic(() => import('react-hot-toast').then(mod => mod.Toaster), { ssr: false });
@@ -15,6 +18,7 @@ function ProfileContent() {
   const [error, setError] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [totalEvents, setTotalEvents] = useState(0); // State for total number of events
   const [searchQuery, setSearchQuery] = useState("");
   const router = useRouter();
 
@@ -71,7 +75,8 @@ function ProfileContent() {
 
           const eventsData = await eventsResponse.json();
           setEvents(eventsData.events);
-          setTotalPages(Math.ceil(eventsData.total / 10)); // Assuming 10 events per page
+          setTotalPages(Math.ceil(eventsData.totalEvents / 10)); // Assuming 10 events per page
+          setTotalEvents(eventsData.totalEvents); // Set total number of events
         } catch (error) {
           console.error('Profile fetch error:', error);
           setError('An error occurred while fetching profile and events');
@@ -110,6 +115,7 @@ function ProfileContent() {
             },
           });
           setEvents(events.filter(event => event.id !== eventId));
+          setTotalEvents(totalEvents - 1); // Decrease the total number of events
         } else {
           const errorData = await response.json();
           setError(errorData.message);
@@ -186,16 +192,26 @@ function ProfileContent() {
   }
 
   if (!user) {
-    return <div className=" d-flex justify-content-center">
-    <Spinner />
-    </div>;
+    return <div className="d-flex justify-content-center"><Spinner /></div>;
   }
+
+  
+  
+
   return (
-    <div className="d-flex">
-      <main className="flex-grow-1 p-3">
-        <div className="pt-3">
-        <Toaster />
+    <div className="container">
+      <main className="">
+      <Toaster />
           <h2 className="text-success">Profile</h2>
+          {/* <section className="row">
+            <div className="col-md-3 col-lg-3 col-sm-2 rounded shadow  bg-white">
+            <div className="align-items-center font-lg">
+            <i className="fa-solid fa-calendar-days"></i>
+            </div>
+            <p className="text-success">Total Events: {totalEvents}</p>
+            </div>
+          </section>
+          <h5>Total Events: </h5>
                 <div className="container  h-100">
                   <div className="row d-flex justify-content-start align-items-center h-100">
                     <div className="col col-lg-8 mb-lg-0">
@@ -241,7 +257,7 @@ function ProfileContent() {
                                           className="rounded-circle p-1 border d-flex justify-content-center align-items-center"
                                           style={{ width: '30px', height: '30px' }}
                                         >
-                                          {/* <Calendar size={70} /> */}
+                                          
                                         </div>
                                         <div className="flex-grow-1 ms-3">
                                           <p className="mt-0">Media heading</p>
@@ -276,8 +292,121 @@ function ProfileContent() {
                       </div>
                     </div>
                   </div>
+                </div> */}
+                <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+                  <div className="col mt-3 mb-2 ">
+                    <div className="card radius-10 border-start border-0 border-4 border-info">
+                      <div className="card-body  rounded shadow  bg-white">
+                        <div className="d-flex align-items-center">
+                          <div className="">
+                            <p className="mb-0 text-secondary">Total Events</p>
+                            <h4 className="my-1 text-info">{totalEvents}</h4>
+                            <p className="mb-0 font-13">All your events</p>
+                          </div>
+                          <div className="widgets-icons-2 rounded-circle bg-gradient-blues ms-auto" style={{ background: 'linear-gradient(to right, #66d2e1,#66d2e1)' }}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="80" // Adjusted width
+                            height="80" // Adjusted height
+                            viewBox="0 0 24 27" // Original viewBox
+                            style={{ fill: 'rgba(255, 255, 255, 1)' }} // Solid black color
+                            >
+                            <path d="M21 20V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2zM9 18H7v-2h2v2zm0-4H7v-2h2v2zm4 4h-2v-2h2v2zm0-4h-2v-2h2v2zm4 4h-2v-2h2v2zm0-4h-2v-2h2v2zm2-5H5V7h14v2z" />
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col mt-3 mb-2">
+                    <div className="card radius-10 border-start border-0 border-4 border-danger">
+                      <div className="card-body rounded shadow  bg-white">
+                        <div className="d-flex align-items-center">
+                          <div>
+                            <p className="mb-0 text-secondary">Click rate</p>
+                            <h4 className="my-1 text-danger">$84,245</h4>
+                            <p className="mb-0 font-13">Average clicks</p>
+                          </div>
+                          <div className="widgets-icons-2 rounded-circle  text-white ms-auto" style={{ background: 'linear-gradient(to right,  #00c6ff, #0072ff)' }}>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="80" // Adjusted width
+                              height="80" // Adjusted height
+                              viewBox="0 0 24 25" // Original viewBox
+                              style={{ fill: 'rgba(255, 255, 255, 1)' }} // Solid black color
+                            >
+                              <path d="M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.32 2.393-2.2 4.106-2.2h2c1.714 0 3.209.88 4.106 2.2C15.828 18.14 14.015 19 12 19s-3.828-.86-5.106-2.228z" />
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col mt-3 mb-2">
+                    <div className="card radius-10 border-start border-0 border-4 border-success">
+                      <div className="card-body rounded shadow  bg-white">
+                        <div className="d-flex align-items-center">
+                          <div>
+                            <p className="mb-0 text-secondary">Followers</p>
+                            <h4 className="my-1 text-success">34.6k</h4>
+                            <p className="mb-0 font-13">All followers</p>
+                          </div>
+                          <div className="widgets-icons-2 rounded-circle bg-gradient-ohhappiness text-white ms-auto bg-success " style={{ background: 'linear-gradient(to right, #009B77, #56ab2f, #E2D200)' }}>
+                          <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="80"
+                              height="80"
+                              viewBox="0 0 20 30"
+                              style={{ fill: 'rgba(255, 255, 255, 1)' }} // Solid black color
+                            >
+                              <path d="M3 3v17a1 1 0 0 0 1 1h17v-2H5V3H3z" />
+                              <path d="M15.293 14.707a.999.999 0 0 0 1.414 0l5-5-1.414-1.414L16 12.586l-2.293-2.293a.999.999 0 0 0-1.414 0l-5 5 1.414 1.414L13 12.414l2.293 2.293z" />
+                            </svg>
+                                                      {/* <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="60"
+                                height="60"
+                                viewBox="0 0 20 30"
+                                style={{ fill: 'rgba(255, 255, 255 )' }} // Corrected inline style
+                              >
+                              <path d="M2 20h20v2H2z" />
+                              <rect x="5" y="2" width="6" height="16" rx="1" />
+                              <rect x="13" y="6" width="6" height="12" rx="1" />
+                            </svg> */}
+
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col mt-3 mb-2">
+                    <div className="card radius-10 border-start border-0 border-4 border-warning">
+                      <div className="card-body rounded shadow  bg-white">
+                        <div className="d-flex align-items-center">
+                          <div>
+                            <p className="mb-0 text-secondary">All attending</p>
+                            <h4 className="my-1 text-warning">8.4K</h4>
+                            <p className="mb-0 font-20"><a href="/events" className="text-decoration-none text-dark" title="see all events">See events</a></p>
+                          </div>
+                          <div className="widgets-icons-2 rounded-circle ms-auto" style={{ background: 'linear-gradient(to right, #ff7f50, #ff8c00, #ffa500)' }}>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="80" // Adjusted width
+                              height="80" // Adjusted height
+                              viewBox="0 0 24 27" // Original viewBox
+                              style={{ fill: 'rgba(255, 255, 255, 1)' }} // Solid black color
+                            >
+                              <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zm6-3.586-3.707-3.707 1.414-1.414L11 15.586l4.293-4.293 1.414 1.414L11 18.414zM5 7h14v2H5V7z" />
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="container py-5 h-100 rounded shadow-sm card">
+
+
+                {/* <div className="container py-5 h-100 rounded shadow-sm card">
               
                 <h6 className="text-primary">Events</h6>
                   <div className="table-responsive">
@@ -427,8 +556,7 @@ function ProfileContent() {
                       </div>
                     </div>
                   </div>
-                  </div>
-            </div>   
+                </div> */}
       </main>
     </div>
   );
